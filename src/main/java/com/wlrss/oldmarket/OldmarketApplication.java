172a9748ea853d01fb1fcbb3964c316b6105c019
abc <1453370401@qq.com>
2020-06-30 15:18:31 +0800

    再次尝试

---
+++ src/main/java/com/wlrss/oldmarket/OldmarketApplication.java
@@ -0,0 +1,13 @@
+package com.wlrss.oldmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldmarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wlrss/oldmarket/oldmarket/OldmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlrss.oldmarket.oldmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
